882ccf1f327c885db7abdf8a3b4221571a04037a
Elsa S <elsasalinas2388@gmail.com>
2025-10-22 10:32:22 -0400

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
+import App from "./App"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* BrowserRouter allows our pages to change without reloading */}
     <BrowserRouter>
-      <App />
+    <App />
     </BrowserRouter>
   </React.StrictMode>
 );
